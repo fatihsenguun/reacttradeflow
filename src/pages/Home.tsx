d52caa8bfaf0924e76d80c6db9b95f11e2fa1e7e
fatihsenguun <fatihsenguun@gmail.com>
2026-01-21 21:20:22 +0300

    grids

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GoBriefcase } from "react-icons/go";
+import { GoBriefcase, GoPackage, GoInbox } from "react-icons/go";
 
 function Home() {
   return (
@@ -16,15 +16,43 @@ function Home() {
           </div>
 
           <div className='col-span-2 flex flex-col overflow-hidden'>
-            <span className='text-sm font-medium text-white truncate'>Total Sales</span>
-            <span className='text-xs text-slate-400'>12.400</span>
+            <span className='text-sm text-slate-300 font-medium  truncate'>Total Sales</span>
+            <span className='text-lg text-teal-300 font-bold'>$ 12.400</span>
           </div>
 
 
 
         </div>
-        <div className='bg-slate-800 h-26 rounded-lg shadow-sm'></div>
-        <div className='bg-slate-800 h-26 rounded-lg shadow-sm'></div>
+        <div className='grid grid-cols-3 bg-slate-800 h-26 rounded-lg shadow-sm flex items-center justify-center hover:bg-slate-700 cursor-pointer transition-colors  gap-4 p-3 '>
+
+
+          <div className='col-span-1 w-11 h-11 rounded-full bg-slate-600 flex items-center justify-center shrink-0'>
+            <span className='text-sm font-bold text-white'><GoInbox className='text-2xl font-2xl' /></span>
+          </div>
+
+          <div className='col-span-2 flex flex-col overflow-hidden'>
+            <span className='text-sm font-medium text-white truncate'>Orders</span>
+            <span className='text-lg text-sky-300 font-bold'>210</span>
+          </div>
+
+
+
+        </div>
+        <div className='grid grid-cols-3 bg-slate-800 h-26 rounded-lg shadow-sm flex items-center justify-center hover:bg-slate-700 cursor-pointer transition-colors  gap-4 p-3 '>
+
+
+          <div className='col-span-1 w-11 h-11 rounded-full bg-slate-600 flex items-center justify-center shrink-0'>
+            <span className='text-sm font-bold text-white'><GoPackage className='text-2xl font-2xl' /></span>
+          </div>
+
+          <div className='col-span-2 flex flex-col overflow-hidden'>
+            <span className='text-sm font-medium text-white truncate'>Total Items</span>
+            <span className='text-lg text-yellow-300 font-bold'>342</span>
+          </div>
+
+
+
+        </div>
 
         {/* 2. Satır: Büyük Geniş Kutu (3 kolonu da kaplar) */}
         <div className='lg:col-span-3 bg-slate-800 h-120 rounded-lg shadow-sm'></div>
